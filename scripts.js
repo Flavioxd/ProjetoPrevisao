401b01ea0de00f29386a79b1f81625b9d9bd153c
Flávio Batista <flavio.18@outlook.com>
2023-02-09 17:15:56 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// VARIÁVEIS => Um espaço da memória do computador que guardamos algo (um numero, uma letra, um texto, uma imagem)
+// FUNÇÃO => Um trecho de código que só é executado quando é chamado
+
+let chave = "cebcd482eda57fa9a6714c1c2ba91885"
+
+
+function colocarNaTela(dados){
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".vento").innerHTML = "Vento :" + Math.floor(dados.wind.speed) + "km/h"
+    document.querySelector(".umidade").innerHTML = "Umidade :" + Math.floor(dados.main.humidity) + "%"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone-1").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    
+}
+
+
+async function buscarCidade(cidade){
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade + 
+    "&appid=" + 
+    chave + 
+    "&lang=pt_br" +
+    "&units=metric"
+    )
+    .then(resposta => resposta.json())
+
+    colocarNaTela(dados)
+}
+
+
+function cliqueiNoBotao(){
+
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
+
+function handle(e){
+  if(e.keyCode === 13){
+      e.preventDefault(); // Ensure it is only this code that runs
+       cliqueiNoBotao();    
+  }
+}
